ccf19aac7315ebdb63590e525089c9203b9f5b41
costadaniele <santoscostadaniele@gmail.com>
2021-08-17 00:14:42 -0300

    Initial commit

---
+++ src/app/hello.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `
+    <h1>{{ name }}</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+        color: green;
+      }
+    `
+  ]
+})
+export class HelloComponent {
+  @Input() name: string;
+}
